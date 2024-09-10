762088fbe9552c5e3e84d9171052fdfcade47904
shuvhanjalAdhikari <135438079+shuvhanjalAdhikari@users.noreply.github.com>
2024-09-10 14:07:42 +0545

    Weather App completed

--- src/components/WeatherUi.jsx
+++ src/components/WeatherUi.jsx
@@ -1,35 +1,95 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useRef, useState } from "react";
+import sun_icon from '../assets/sun.png'
+import cloud_icon from '../assets/clouds.png'
+import rain_icon from '../assets/raining.png'
+import snow_icon from '../assets/snowflake.png'
 import { FaWind } from "react-icons/fa";
 import { WiHumidity } from "react-icons/wi";
-import { IoIosSunny } from "react-icons/io";
 import { FaSearch } from "react-icons/fa";
 
 const WeatherUi = () => {
+  const [weatherData, setWeatherData] = useState(false);
+  const allIcons={
+    "01d":sun_icon,
+    "01n":sun_icon,
+    "02d":cloud_icon,
+    "02n":cloud_icon,
+    "03d":cloud_icon,
+    "03n":cloud_icon,
+    "04d":rain_icon,
+    "04n":rain_icon,
+    "09d":rain_icon,
+    "09n":rain_icon,
+    "10d":rain_icon,
+    "10n":rain_icon,
+    "13d":snow_icon,
+    "13n":snow_icon,
+  }
 
-    const [Temp, setTemp]=useState(10)
-    const [Search, setSearch]=useState("")
+  const inputRef = useRef();
 
-    const handleSearch=(e)=>{
-        setSearch(e.target.value)
-    }
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const icon=allIcons[data.weather[0].icon] || sun_icon
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon:icon
+      });
+    } catch (error) {}
+  };
 
+  useEffect(() => {
+    search("");
+  }, []);
 
   return (
-    <div className='h-screen w-full bg-gray-300'>
-      <div className=' flex absolute bg-white ml-52  mt-20 w-[80vw] h-[80vh] rounded-3xl py-96'>
-        <input type="text" className='absolute  shadow-lg -mt-72 ml-24 px-10 py-3 border-2 border-black rounded-2xl' placeholder='Search...' value={Search} onChange={handleSearch} /><FaSearch className='absolute -mt-[273px] text-lg ml-[340px] cursor-pointer'/>
-        <div className='flex -mt-96'>
-            <h1 className=' mt-64 ml-20 text-6xl'>Kathmandu</h1>
-            <h1 className='mt-16 text-4xl ml-60'>Weather App</h1>
-            <h1 className='absolute text-9xl mt-64 ml-[42rem]'>{Temp}<span className='-mt-5 -ml-2 absolute text-7xl'> &deg; </span> C</h1>
-            <h1 className=' absolute text-2xl mt-[30rem] ml-[40rem]'><span><WiHumidity className='text-4xl' /></span><span>91%</span><br />Humidity</h1>
-            <h1 className='text-2xl mt-[30rem] mr-24'><span><FaWind className='text-4xl'/></span><span>3.4 Km/Hr</span><br />Wind Speed</h1>   
+    <div className="h-screen w-full bg-gray-300">
+      <div className=" flex absolute bg-white ml-52  mt-20 w-[80vw] h-[80vh] rounded-3xl py-96">
+        <input
+          ref={inputRef}
+          type="text"
+          className="absolute  shadow-lg -mt-72 ml-24 px-10 py-3 border-2 border-black rounded-2xl"
+          placeholder="Search..."
+        />
+        <FaSearch
+          className="absolute -mt-[273px] text-lg ml-[340px] cursor-pointer"
+          onClick={() => search(inputRef.current.value)}
+        />
+        <div className="flex -mt-96">
+          <h1 className=" mt-80 ml-20 text-6xl">{weatherData.location}</h1>
+          <h1 className=" absolute mt-16 text-4xl ml-[40rem]">Weather App</h1>
+          <h1 className="absolute text-9xl mt-64 ml-[42rem]">
+            {weatherData.temperature}
+            <span className="-mt-5 -ml-2 absolute text-7xl"> &deg; </span> C
+          </h1>
+          <h1 className=" absolute text-2xl mt-[30rem] ml-[43rem]">
+            <span>
+              <WiHumidity className="text-4xl" />
+            </span>
+            <br />
+            {weatherData.humidity}%
+          </h1>
+          <h1 className=" absolute text-2xl mt-[30rem] ml-[53rem]">
+            <span>
+              <FaWind className="text-4xl" />
+            </span>
+            <br />
+            {weatherData.windSpeeed} Km/Hr
+          </h1>
         </div>
-        <IoIosSunny className='text-[300px]  absolute -mt-52 ml-[70rem] text-yellow-500' />
+        <img src={weatherData.icon} alt="" className=" absolute h-[300px] w-[300px] object-contain ml-[69rem] -mt-44"/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherUi
+export default WeatherUi;
